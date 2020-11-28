80c507ee79538cc2ce7c0acbe8a3a21f2e2bffb3
emmasagot <emma.sagot@eivp-paris.fr>
2020-11-28 22:36:17 +0100

    nouveau code

--- Projet_Programmation_SAGOT_Emma_HAMID_Ikram.py
+++ Projet_Programmation_SAGOT_Emma_HAMID_Ikram.py
@@ -5,42 +5,44 @@ import matplotlib.pyplot as plt
 import csv
 
 data = pd.read_csv (r'/Users/mac/Desktop/EIVP/Algo et programmation/EIVP_KM.csv', sep = ';')
-# print(data)
+#print(data)
 
+tableau_data=data.values #conversion du fichier csv en tableau numpy
 
-## Affichage graphique température en fonction du temps
+## Affichage d'un graphique temp=f(sent_at)
 
-sent_at=[] #liste contenant tous les echantillons de temps
-temp=[] #liste contenat tous les echantillons de température
+x=[]
+y=[]
 
-#code permettant de "remplir" les listes précedemment crées :
-with open('/Users/mac/Desktop/EIVP/Algo et programmation/EIVP_KM.csv', 'r') as csvfile:
-    plots= csv.reader(csvfile, delimiter=';')
-    for i in plots:
-        sent_at.append(i[6])
-        temp.append(i[2])
+#récupération des colonnes qui nous intéressent
+
+temp=[]
+sent_at=[]
+
+for i in range (len(tableau_data)):
+    temp.append(tableau_data[i][2])
+    sent_at.append(tableau_data[i][6])
 
 #fonction permettant de réduire l'intervalle d'étude en définissant la date de début (a) et la date de fin (b)
+
 def intervalle(a,b):
     abs=[]
     ord=[]
-    for i in range(sent_at.index(a),sent_at.index(b)):
+    for i in range (sent_at.index(a),sent_at.index(b)):
         abs.append(sent_at[i])
         ord.append(temp[i])
-    return abs,ord
+    return abs, ord
 
 #affichage graphique pour valeurs choisies
-a='2019-08-21 11:46:04 +0200' #date de début choisie
+
+a='2019-08-20 11:46:11 +0200' #date de début choisie
 b='2019-08-25 11:45:54 +0200' #date de fin choisie
 x,y=intervalle(a,b)
 
 plt.plot(x,y, marker='o')
-
-plt.title('Data from the CSV File: temp and time')
-
-plt.xlabel('time')
-plt.ylabel('temp')
-
+plt.xlabel('temps')
+plt.ylabel('température')
+plt.title('température en fonction du temps')
 plt.show()
 
 
